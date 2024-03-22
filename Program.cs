f5de60a5e674d42a2657958b600c008b411cdfd9
Поленникова Ольга <olga.polennikova@inside.glowbyte.com>
2024-03-22 14:04:21 +0300

    Добавлено решение 1-ой задачи

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 1. Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N. 
+// Использовать рекурсию, не использовать циклы.
+
+void PrintDigits(int M, int N)
+{
+    if(N < M)
+    {
+        return;
+    }
+    PrintDigits(M, N - 1);
+    Console.Write(N);
+    Console.Write(" ");
+}
+
+PrintDigits(1, 5);

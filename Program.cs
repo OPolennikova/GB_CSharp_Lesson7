851f31bd923242918e90cfd4227f4ab34c42e614
Поленникова Ольга <olga.polennikova@inside.glowbyte.com>
2024-03-22 15:51:56 +0300

    Добавлено решение 2 и 3 задач

--- Program.cs
+++ Program.cs
@@ -13,3 +13,47 @@ void PrintDigits(int M, int N)
 }
 
 PrintDigits(1, 5);
+Console.WriteLine();
+
+
+// Задача 2. Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n
+
+int AckFunction(int m, int n)
+{
+    if(m == 0)
+    {
+        return n + 1;
+    }
+    else if (m > 0 && n == 0)
+    {
+        return AckFunction(m - 1, 1);
+    }
+    else
+    {
+        return AckFunction(m - 1, AckFunction(m, n - 1));
+    }
+}
+
+int result = AckFunction(2, 3);
+Console.Write(result);
+Console.WriteLine();
+
+
+
+// Задача 3. Задайте произвольный массив. Выведете его элементы, начиная с конца. Использовать рекурсию, не использовать циклы
+
+void PrintArrayReversed(int[] array, int index)
+{
+    if(index < 0)
+    {
+        return;
+    }
+
+    Console.Write(array[index] + " ");
+    PrintArrayReversed(array, index - 1);
+}
+
+
+int [] array = {1, 2, 5, 0, 10, 34};
+PrintArrayReversed(array, array.Length - 1);
+Console.WriteLine();
